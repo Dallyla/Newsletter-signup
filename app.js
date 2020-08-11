7ab3bfae6f35319476d36c96d49702cdee8fb998
Dallyla Oliveira <dallyla.s.oliveira@gmail.com>
2020-08-10 22:14:20 -0300

    Adding Success and Failure Pages

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ app.post("/", function(req, res) {
     }
 
     const request = https.request(url, options, function(response) {
+        
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        
         response.on("data", function(data) {
             console.log(JSON.parse(data));
         })
@@ -54,8 +61,10 @@ app.post("/", function(req, res) {
 
 });
 
+app.post("/failure", function (req, res){
+    res.redirect("/");
+})
 
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("The server is up and running on port 3000");
 })
